458dfa126e44d26bf33c357963ec67dd520eb928
Alissa Garcia <118315759+AlissaGarcia@users.noreply.github.com>
2026-01-17 22:51:01 +0000

    libraries

--- src/login.py
+++ src/login.py
@@ -1,4 +1,3 @@
-#Código de Alissa Garcia Moreira - ADS - S3
-#Sistema de Gerenciamento de Clínica Odontológica
-#SmileLab - Todos os Direitos Reservados
-
+# Código de Alissa Garcia Moreira - ADS - S3
+# Sistema de Gerenciamento de Clínica Odontológica
+# SmileLab - Todos os Direitos Reservados
